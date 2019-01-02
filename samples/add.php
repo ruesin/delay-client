1dc04790ab0ae7098fa9823d22b96b2b0418f90c
ruesin <ruesin@gmail.com>
2019-01-02 21:16:41 +0800

    fix samples bug

--- samples/add.php
+++ samples/add.php
@@ -8,6 +8,6 @@ $instance = \DlyCli\Client::getInstance($config);
 
 $result = $instance->sendMessage('ruesin', 'This is My first message. Delay active 5 seconds.', 5);
 
-$instance->sendMessage('ruesin', json_encode(['id'=>123,'platform_id'=>95]), JSON_UNESCAPED_UNICODE);
+$instance->sendMessage('ruesin', json_encode(['id'=>123,'platform_id'=>95], JSON_UNESCAPED_UNICODE));
 
 var_dump($result);
--- samples/config/config.php
+++ samples/config/config.php
@@ -5,5 +5,6 @@ require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
 return [
     'access_id'  => 'ruesin',
     'access_key' => 'ruesin_key',
-    'end_point'  => 'http://local.delay.com'
+    //'end_point' => 'http://local.delay.com',
+    'end_point' => 'http://127.0.0.1:9501',
 ];
